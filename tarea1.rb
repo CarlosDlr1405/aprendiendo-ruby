22288b895b98fde754d5b863374a967781719d68
carlos de la rosa <cdelarosa@ucol.mx>
2021-08-12 12:26:55 -0500

    aprendiendo abstraccion en ruby

---
+++ tarea1.rb
@@ -0,0 +1,43 @@
+class Animales
+  def initialize(nombre, color, tamaño)
+    @nombre = nombre
+    @color = color
+    @tamaño = tamaño
+      
+  end
+  
+  def caminar
+    print "Hola soy ", @nombre , ", mi color es ", @color, " , mi tamaño es ", @tamaño, " y voy caminando"
+  end
+      
+  def correr
+    print "Hola soy ", @nombre , ", mi color es ", @color, " , mi tamaño es ", @tamaño, " y voy corriendo"
+  end
+
+end
+  
+class Perro < Animales
+  def correr
+  super
+    print (", Guuaau")
+  end
+  def caminar
+  super
+    print (", Guuaau")
+  end
+end
+  
+class Gato < Animales
+  def caminar
+  super
+    print (", Miaaaauu")
+  end
+  def correr
+  super
+    print (", Miaaaauu")
+  end
+end
+  
+datos = Perro.new("ansi", "negro","pequeño")
+print datos.correr
+  
